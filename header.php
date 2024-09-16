b391c46efa50f0de3b95ef384e17896a14870554
dansan-dsn <ddryn970@gmail.com>
2024-09-17 01:25:55 +0300

    delete - show data

--- header.php
+++ header.php
@@ -16,6 +16,8 @@
     <link rel="stylesheet" href="assets/css/demo.css">
     <link rel="stylesheet" href="assets/css/header.products.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
     <style>
         a {
--- new-products.php
+++ new-products.php
@@ -2,28 +2,16 @@
 <div class="main-panel">
 	<div class="content">
 	    <div class="container-fluid">
-
-        <?php
+<?php
 include './db_connection.php'; 
 
-$product_name = '';
-$purchase_order_no = '';
-$delivery_order_no = '';
-$category = '';
-$supplier = '';
-$accounting_code = '';
-$quantity_received ='';
-$units = '';
-$location = '';
-$description = '';
-$message = '';
-$success = '';
+$product_name = $purchase_order_no = $delivery_order_no = $category = $supplier = $accounting_code = $quantity_received = $units = $location = $description = $message = $success = '';
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $product_name = $_POST['product_name'];
     $purchase_order_no = $_POST['purchase_order_no'];
-    $delivery_order_no = $_POST['delivery_order_no'];
+    $delivery_order_no = $_POST['deliverys_order_no'];
     $category = $_POST['category'];
     $supplier = $_POST['supplier'];
     $accounting_code = $_POST['accounting_code'];
@@ -43,16 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
             if ($stmt->execute()) {
                 $success = "Product Added Successfully";
-                $product_name = '';
-                $purchase_order_no = '';
-                $delivery_order_no = '';
-                $category = '';
-                $supplier = '';
-                $accounting_code = '';
-                $quantity_received = '';
-                $units = '';
-                $location = '';
-                $description = '';
+                $product_name = $purchase_order_no = $delivery_order_no = $category = $supplier = $accounting_code = $quantity_received = $units = $location = $description = '';
             } else {
                 $message = "Failed to add Product";
             }
@@ -63,15 +42,51 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 
-$conn->close();
+// all product
+$products = "SELECT * FROM product_tb";
+$result = $conn->query($products);
+
+$index = 1;
+
+// a single product
+
+//  $id = isset($_GET['id']) ? intval($_GET['id']) : 0;
+
+//  $query = $conn->prepare("SELECT * FROM product_tb WHERE id = ?");
+//  $query->bind_param("i", $id);
+//  $query->execute();
+//  $single_result = $query->get_result();
+
+//  if($result->num_rows > 0) {
+//     $product = $result->fetch_assoc();
+//  }
+
+if (isset($_GET['delete_id'])) {
+    $delete_id = intval($_GET['delete_id']);  // Ensure delete_id is an integer
+
+    // Prepare the DELETE query
+    $delete_stmt = $conn->prepare("DELETE FROM product_tb WHERE id = ?");
+    if ($delete_stmt) {
+        $delete_stmt->bind_param("i", $delete_id);  // Bind the product ID
+        if ($delete_stmt->execute()) {
+            $success = "Product deleted successfully";
+        } else {
+            $message = "Failed to delete product";
+        }
+        $delete_stmt->close();
+    } else {
+        $message = "Failed to prepare the delete SQL statement.";
+    }
+}
+
 ?>
 
                 <?php if (!empty($success)): ?> 
-                    <div class="d-flex justify-content-center row">
+                    <div class="d-flex justify-content-center row " id="success-m">
                         <p class="bg-success p-2 text-center text-white rounded col-12 col-sm-6 col-md-4"><?php echo htmlspecialchars($success); ?></p>
                     </div>
                 <?php elseif (!empty($message)): ?>
-                    <div class="d-flex justify-content-center row">
+                    <div class="d-flex justify-content-center row " id="error-m">
                         <p class="bg-danger p-2 text-center text-white rounded col-12 col-sm-6 col-md-4"><?php echo htmlspecialchars($message); ?></p>
                     </div>
                 <?php endif; ?>
@@ -81,9 +96,9 @@ $conn->close();
                 <button class="btn btn-info">Scan OCR</button>
                 <button class="btn btn-outline-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseForm" aria-expanded="false" aria-controls="collapseForm"><span class="fs-5 mx-2">&plus;</span>New Product</button>
             </div>
-            <form action="" method="POST" class="collapse border p-3 rounded-4 m-3 input-bg" id="collapseForm">
+            <form action="" method="POST" class="collapse border p-3 rounded-4 my-3 input-bg" id="collapseForm">
                 <div class="row mb-3">
-                    <div class="col-md-4 mb-3 mb-md-0 has">
+                    <div class="col-md-4 mb-3 mb-md-0 ">
                         <label for="ProductName" class="form-label" value="<?php echo htmlspecialchars($product_name); ?>" required>Product Name</label>
                         <input type="text" class="form-control " name="product_name" id="ProductName" placeholder="Product Name" required>
                     </div>
@@ -93,7 +108,7 @@ $conn->close();
                     </div>
                     <div class="col-md-4">
                         <label for="DeliveryOrderNo" class="form-label">Delivery Order No.</label>
-                        <input type="text" class="form-control" name="delivery_order_no" value="<?php echo htmlspecialchars($delivery_order_no); ?>" id="DeliveryOrderNo" placeholder="MN234" requied>
+                        <input type="text" class="form-control" name="delivery_order_no" value="<?php echo htmlspecialchars($delivery_order_no); ?>" id="DeliveryOrderNo" placeholder="MN234" required>
                     </div>
                 </div>
         
@@ -161,66 +176,138 @@ $conn->close();
                     </div>
                 </div>
             </form>
+            <?php if ($result->num_rows > 0): ?>
             <table class="table table-striped mt-4">
                 <thead>
-                    <tr>
-                        <th scope="col" class="">No</th>
-                        <th scope="col" class="fs-6">Product</th>
-                        <th scope="col">Product ID</th>
-                        <th scope="col">Category<span class="mx-2 caret"></th>
-                        <th scope="col">Location<span class="mx-2 caret"></span>
-                        <th scope="col">Available</th>
-                        <th scope="col">Quantity Received</th>
-                        <th scope="col">Units</th>
+                    <tr class="fs-sm">
+                        <th scope="col">No</th>
+                        <th scope="col" >Product</th>
+                        <th scope="col">Supplier</th>
+                        <th scope="col" >Category&#9660;</th>
+                        <th scope="col" class="pointer-cursor">Location&#9660;</th>
+                        <th scope="col">Accounting Code</th>
                         <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody >
+                    <?php while($row = $result->fetch_assoc()): ?>
                         <tr>
-                        <th scope="row">1</th>
-                        <td>IPhpne 12</td>
-                        <td>IM31</td>
-                        <td>Phones</td>
-                        <td>Warehouse1</td>
-                        <td class="s-md">234</td>
-                        <td class="">12</td>
-                        <td>111</td>
-                        <td>
-                            <i class='bx bxs-show px-1 bg-info text-white p-1 rounded-pill hover-sicon' title="show"></i>
+                        <th scope="row"><?php echo $index++;?></th>
+                        <td><?php echo htmlspecialchars($row['product_name']); ?></td>
+                        <td><?php echo htmlspecialchars($row['supplier']); ?></td>
+                        <td><?php echo htmlspecialchars($row['category']); ?></td>
+                        <td><?php echo htmlspecialchars($row['location']); ?></td>
+                        <td><?php echo htmlspecialchars($row['accounting_code']); ?></td>
+                        <td class="d-flex">
+                            <i class='bx bxs-show px-1 bg-info text-white p-1 rounded-pill hover-sicon' title="show" data-bs-toggle="modal" data-bs-target="#showModal<?php echo $index; ?>"></i>
+
+                            <!-- <?php 
+                            // if( $single_result->num_rows > 0):
+                                 ?> -->
+                            <div class="modal fade" id="showModal<?php echo $index?>" tabindex="-1" aria-labelledby="showModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title text-primary-emphasis" id="showMoalLabel"><?php echo htmlspecialchars($row['product_name']); ?></h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            
+                                        </div>
+                                        
+                                        <div class="modal-body">
+                                            <div class="mb-2 mx-3">
+                                                <div class="d-flex justify-content-between">
+                                                    <h6>Purchase No: </h6>
+                                                    <p class="fs-6"><?php echo htmlspecialchars($row['purchase_order_no']); ?></p>
+                                                </div>
+                                                <div class="d-flex justify-content-between">
+                                                    <h6>Delivery No: </h6>
+                                                    <p class="fs-6"><?php echo htmlspecialchars($row['delivery_order_no']); ?></p>
+                                                </div>
+                                                <div class="d-flex justify-content-between">
+                                                    <h6>Accounting Code: </h6>
+                                                    <p class="fs-6"><?php echo htmlspecialchars($row['accounting_code']); ?></p>
+                                                </div>
+                                                <div class="d-flex justify-content-between">
+                                                    <h6>Supplier: </h6>
+                                                    <p class="fs-6"><?php echo htmlspecialchars($row['supplier']); ?></p>
+                                                </div>
+                                                <div class="d-flex justify-content-between">
+                                                    <h6>Category: </h6>
+                                                    <p class="fs-6"><?php echo htmlspecialchars($row['category']); ?></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
                             <!-- Edit icon -->
-                            <i class='bx bxs-edit-alt px-1 bg-primary text-white p-1 mx-1 rounded hover-eicon' title="Edit"></i>
+                            <i class='bx bxs-edit-alt px-1 bg-primary text-white p-1 mx-1 rounded hover-eicon' 
+                            title="Edit" 
+                            data-bs-toggle="modal" 
+                            data-bs-target="#editModal"></i>
+
+                                <!-- Modal for Edit-->
+                                <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="editModalLabel">Edit Product</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form>
+                                        <div class="mb-3">
+                                            <label for="itemName" class="form-label">Item Name</label>
+                                            <input type="text" class="form-control" id="itemName" placeholder="Enter item name">
+                                        </div>
+                                        </form>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-primary">Edit</button>
+                                    </div>
+                                    </div>
+                                </div>
+                                </div>
+
                             <!-- Delete icon -->
-                            <i class='bx bx-trash bg-danger text-white p-1 rounded hover-dicon' title="Delete"></i>
+                            <a href="?delete_id=<?php echo $row['id'];?>" onclick="return confirm('Are you sure to delete this product')"><i class='bx bx-trash bg-danger text-white p-1 rounded hover-dicon' title="Delete"></i></a>
                         </td>
                         </tr>
-                    <tr>
-                    <th scope="row">2</th>
-                    <td>Hp Pavilion</td>
-                    <td>IM32</td>
-                    <td>Laptops</td>
-                    <td>Warehouse2</td>
-                    <td>123</td>
-                    <td>234</td>
-                    <td>2345</td>
-                    <td>
-                        <!-- view icon -->
-                        <i class='bx bxs-show px-1 bg-info text-white p-1 rounded-pill hover-sicon' title="show"></i>
-                        <!-- Edit icon -->
-                        <i class='bx bxs-edit-alt px-1 bg-primary text-white p-1 mx-1 rounded hover-eicon' title="Edit"></i>
-                        <!-- Delete icon -->
-                        <i class='bx bx-trash bg-danger text-white p-1 rounded hover-dicon' title="Delete"></i></td>
-                    </tr>
+                        <?php endwhile;?>
                 </tbody>
             </table>
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">Product</h5>
-                  <button type="button" class="btn-close bg-light" data-bs-dismiss="modal" aria-label="Close">&times;</button>
-                </div>
-                <div class="modal-body">
-        </div>
+            <?php else: ?>
+                <p class="text-center mt-5 fs-5 fw-bold">No Product</p>
+            <?php endif;?>
+            <?php
+            $result->free();
+            $conn->close();
+            ?>
     </div>
 </div>
 <?php include './footer.php'; ?>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const successMessage = document.getElementById('success-m');
+        if (successMessage) {
+            setTimeout(function() {
+                successMessage.style.opacity = '0';
+                setTimeout(function() {
+                    successMessage.style.display = 'none';
+                }, 600); 
+            }, 2000);
+        }
+
+        const errorMessage = document.getElementById('error-m');
+        if (errorMessage) {
+            setTimeout(function() {
+                errorMessage.style.opacity = '0';
+                setTimeout(function() {
+                    errorMessage.style.display = 'none';
+                }, 600); 
+            }, 2000);
+        }
+    });
+</script>
